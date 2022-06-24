58d8dfc684a73cf5ba0e4dc5a1c802f85d58b56b
Kasidis Chantharojwong <me@ckasidis.com>
2022-06-24 19:08:05 +0800

    fix args not passing

--- src/stories/TextField/TextField.stories.tsx
+++ src/stories/TextField/TextField.stories.tsx
@@ -13,23 +13,28 @@ const Template: ComponentStory<typeof TextField> = (args) => (
 export const Blank = Template.bind({});
 Blank.args = {};
 
-const TemplateWithLabel: ComponentStory<typeof TextField> = () => (
+const TemplateWithLabel: ComponentStory<typeof TextField> = (args) => (
 	<ul className="flex flex-col gap-8">
 		<li className="flex flex-row items-end gap-4">
-			<TextField />
-			<TextField label="Label" />
+			<TextField {...args} />
+			<TextField label="Label" {...args} />
 		</li>
 		<li className="flex flex-row items-end gap-4">
-			<TextField placeholder="Placeholder" />
-			<TextField label="Label" placeholder="Placeholder" />
+			<TextField placeholder="Placeholder" {...args} />
+			<TextField label="Label" placeholder="Placeholder" {...args} />
 		</li>
 		<li className="flex flex-row items-end gap-4">
-			<TextField defaultValue={'Input'} />
-			<TextField label="Label" defaultValue={'Input'} />
+			<TextField defaultValue={'Input'} {...args} />
+			<TextField label="Label" defaultValue={'Input'} {...args} />
 		</li>
 		<li className="flex flex-row items-end gap-4">
-			<TextField type={'Password'} defaultValue={'12345'} />
-			<TextField label="Label" type={'Password'} defaultValue={'12345'} />
+			<TextField type={'Password'} defaultValue={'12345'} {...args} />
+			<TextField
+				label="Label"
+				type={'Password'}
+				defaultValue={'12345'}
+				{...args}
+			/>
 		</li>
 	</ul>
 );
@@ -49,29 +54,31 @@ DisabledWithLabel.args = {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const TemplateWithIcon: ComponentStory<typeof TextField> = () => (
+const TemplateWithIcon: ComponentStory<typeof TextField> = (args) => (
 	<ul className="flex flex-col gap-8">
 		<li className="flex flex-row items-end gap-4">
-			<TextField leftIcon={HiOutlineUser} />
-			<TextField rightIcon={HiOutlineX} />
-			<TextField leftIcon={HiOutlineUser} rightIcon={HiOutlineX} />
+			<TextField leftIcon={HiOutlineUser} {...args} />
+			<TextField rightIcon={HiOutlineX} {...args} />
+			<TextField leftIcon={HiOutlineUser} rightIcon={HiOutlineX} {...args} />
 		</li>
 		<li className="flex flex-row items-end gap-4">
-			<TextField leftIcon={HiOutlineUser} placeholder="Placeholder" />
-			<TextField rightIcon={HiOutlineX} placeholder="Placeholder" />
+			<TextField leftIcon={HiOutlineUser} placeholder="Placeholder" {...args} />
+			<TextField rightIcon={HiOutlineX} placeholder="Placeholder" {...args} />
 			<TextField
 				leftIcon={HiOutlineUser}
 				rightIcon={HiOutlineX}
 				placeholder="Placeholder"
+				{...args}
 			/>
 		</li>
 		<li className="flex flex-row items-end gap-4">
-			<TextField leftIcon={HiOutlineUser} defaultValue={'Input'} />
-			<TextField rightIcon={HiOutlineX} defaultValue={'Input'} />
+			<TextField leftIcon={HiOutlineUser} defaultValue={'Input'} {...args} />
+			<TextField rightIcon={HiOutlineX} defaultValue={'Input'} {...args} />
 			<TextField
 				leftIcon={HiOutlineUser}
 				rightIcon={HiOutlineX}
 				defaultValue={'Input'}
+				{...args}
 			/>
 		</li>
 		<li className="flex flex-row items-end gap-4">
@@ -79,17 +86,20 @@ const TemplateWithIcon: ComponentStory<typeof TextField> = () => (
 				leftIcon={HiOutlineUser}
 				type={'Password'}
 				defaultValue={'12345'}
+				{...args}
 			/>
 			<TextField
 				rightIcon={HiOutlineEyeOff}
 				type={'Password'}
 				defaultValue={'12345'}
+				{...args}
 			/>
 			<TextField
 				leftIcon={HiOutlineEyeOff}
 				rightIcon={HiOutlineX}
 				type={'Password'}
 				defaultValue={'12345'}
+				{...args}
 			/>
 		</li>
 	</ul>
